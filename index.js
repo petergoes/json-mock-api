c72e97a845fa53184406ed9b45d66df9feaad6f1
Peter Goes <petergoes@gmail.com>
2018-05-31 22:09:46 +0200

    add bin/json-mock-api

--- index.js
+++ index.js
@@ -6,52 +6,56 @@ const app = express()
 
 const router = Router()
 
-const PORT = process.env.PORT || 8378
+function jsonMockApi(port, dir) {
+  const PORT = port || 3000
+  const FILES_DIR = path.join(process.cwd(), dir) || process.cwd()
 
-const filterFiles = re => files => new RegExp(re).test(files)
+  const filterFiles = re => files => new RegExp(re).test(files)
 
-router.all('*', (req, res) => {
-  const url = req.url
-  const method = req.method.toLowerCase()
-  const [input, folder, file] = /(.+\/)([\w-]+)\/?$/.exec(url)
-  try {
-    const filesInFolder = fs
-      .readdirSync(path.join(__dirname, folder))
-      .filter(filterFiles(file))
-      .filter(filterFiles(`${method}|${file}.[json|txt]`))
+  router.all('*', (req, res) => {
+    const url = req.url
+    const method = req.method.toLowerCase()
+    const [input, folder, file] = /(.+\/)([\w-]+)\/?$/.exec(url)
+    try {
+      const filesInFolder = fs
+        .readdirSync(path.join(FILES_DIR, folder))
+        .filter(filterFiles(file))
+        .filter(filterFiles(`${method}|${file}.[json|txt]`))
 
-    const methodFileJSON = filesInFolder.find(filterFiles(`.${method}.json`))
-    const methodFileText = filesInFolder.find(filterFiles(`.${method}.txt`))
-    const allFileJSON = filesInFolder.find(filterFiles(`${file}.json`))
-    const allFileText = filesInFolder.find(filterFiles(`${file}.txt`))
+      const methodFileJSON = filesInFolder.find(filterFiles(`.${method}.json`))
+      const methodFileText = filesInFolder.find(filterFiles(`.${method}.txt`))
+      const allFileJSON = filesInFolder.find(filterFiles(`${file}.json`))
+      const allFileText = filesInFolder.find(filterFiles(`${file}.txt`))
 
-    if (methodFileJSON) {
-      res.json(require(path.join(__dirname, folder, methodFileJSON)))
-    } else if (methodFileText) {
-      res.send(fs.readFileSync(path.join(__dirname, folder, methodFileText)))
-    } else if (allFileJSON) {
-      res.json(require(path.join(__dirname, folder, allFileJSON)))
-    } else if (allFileText) {
-      res.send(fs.readFileSync(path.join(__dirname, folder, allFileText)))
-    } else {
-      res
-        .status(404)
-        .send({ error: `${input}.${method}.json or ${input}.json not found` })
+      if (methodFileJSON) {
+        res.json(require(path.join(FILES_DIR, folder, methodFileJSON)))
+      } else if (methodFileText) {
+        res.send(fs.readFileSync(path.join(FILES_DIR, folder, methodFileText)))
+      } else if (allFileJSON) {
+        res.json(require(path.join(FILES_DIR, folder, allFileJSON)))
+      } else if (allFileText) {
+        res.send(fs.readFileSync(path.join(FILES_DIR, folder, allFileText)))
+      } else {
+        res
+          .status(404)
+          .send({ error: `${input}.${method}.json or ${input}.json not found` })
+      }
+    } catch (err) {
+      if (new RegExp('no such file').test(err.message)) {
+        res
+          .status(404)
+          .send({ error: `${input}.${method}.json or ${input}.json not found` })
+      } else {
+        res.status(500).send({ error: err.message })
+      }
     }
-  } catch (err) {
-    if (new RegExp('no such file').test(err.message)) {
-      res
-        .status(404)
-        .send({ error: `${input}.${method}.json or ${input}.json not found` })
-    } else {
-      res.status(500).send({ error: err.message })
-    }
-  }
-})
+  })
 
-app.use(router)
+  app.use(router)
 
-app.listen(PORT, () =>
-  process.stdout.write(`Mock server running on port ${PORT}`),
-)
+  app.listen(PORT, () => {
+    console.log(`\n> Mock server running on port http://localhost:${PORT}\n`)
+  })
+}
 
+module.exports = jsonMockApi
